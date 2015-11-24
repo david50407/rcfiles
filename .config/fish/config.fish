f3ace6346b7a5743018d1caaa0bd7f1b6a2c8c69
David Kuo <s50407s@gmail.com>
2015-11-24 23:07:41 +0800

    For Darwin (OS X)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
+set $os (uname)
+
 function fish_prompt
 	~/powerline-shell-clearly.py $status --shell bare ^/dev/null
 end
@@ -21,13 +23,16 @@ function fish_right_prompt -d "Write out the date and time on right prompt"
 end
 
 ### PATH ###
-set -gx ANDROID_HOME ~/Library/Developer/AndroidSDK
-set default_path /usr/bin /usr/sbin /bin /sbin
-set homebrew /usr/local/bin /usr/local/sbin
-set npm_path /usr/local/share/npm/bin
-set android_sdk $ANDROID_HOME/platform-tools/
-set android_sdk_toolchains $ANDROID_HOME/tools/
-set -gx PATH $android_sdk $android_sdk_toolchains $npm_path $homebrew $default_path
+if [ $os = 'Darwin' ]
+	set -gx ANDROID_HOME ~/Library/Developer/AndroidSDK
+	set default_path /usr/bin /usr/sbin /bin /sbin
+	set homebrew /usr/local/bin /usr/local/sbin
+	set npm_path /usr/local/share/npm/bin
+	set android_sdk $ANDROID_HOME/platform-tools/
+	set android_sdk_toolchains $ANDROID_HOME/tools/
+	set -gx PATH $android_sdk $android_sdk_toolchains $npm_path $homebrew $default_path
+	set -gx LIBRARY_PATH $LIBRARY_PATH /usr/local/lib
+end
 
 rvm current 1>/dev/null 2>/dev/null
 bass source ~/.nvm/nvm.sh 1>/dev/null 2>/dev/null
